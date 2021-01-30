c517b230ccbd0481c4a32087ed3ddafb01924d62
kerok 1804 <kerok1804@gmail.com>
2021-01-30 14:27:52 -0300

    end logger

--- stats.js
+++ stats.js
@@ -1,4 +1,5 @@
 const os = require('os')
+const log = require('./logger')
 
 
 setInterval(() => {
@@ -19,5 +20,8 @@ setInterval(() => {
     console.log("====PC STATS====")
     console.table(stats)
 
-}, )
+    log(`${JSON.stringify(stats)}\n`)
+
+
+}, 1000)
 
